3c13ce94beb8fba5d1136012d7308ff865e8d743
Hamas <muaat@192.168.1.7>
2025-03-13 01:53:16 +0300

    Updated StudentGradeTest to JUnit 5

--- test/studentgrade/StudentGradeTest.java
+++ test/studentgrade/StudentGradeTest.java
@@ -1,14 +1,13 @@
 package studentgrade;
 
-import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 /* JUnit 3.8.1 */
-public class StudentGradeTest extends TestCase {
+public class StudentGradeTest {
     
-    public StudentGradeTest(String testName) {
-        super(testName);
-    }
-
+    @Test
+    
     public void testGetGrade95() {
         System.out.println("getGrade 95");
         int mark = 95;
@@ -16,7 +15,7 @@ public class StudentGradeTest extends TestCase {
         String result = StudentGrade.getGrade(mark);
         assertEquals(expResult, result);
     }
-
+    @Test
     public void testGetGrade85() {
         System.out.println("getGrade 85");
         int mark = 85;
@@ -24,11 +23,11 @@ public class StudentGradeTest extends TestCase {
         String result = StudentGrade.getGrade(mark);
         assertEquals(expResult, result);
     }
-    
+    @Test
     public void testGetGrade75() {
-        System.out.println("getGrade 85");
+        System.out.println("getGrade 75");
         int mark = 75;
-        String expResult = "F";
+        String expResult = "C";
         String result = StudentGrade.getGrade(mark);
         assertEquals(expResult, result);
     }
